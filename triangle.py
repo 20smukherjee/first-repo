72a295da94da447187b02e50d82de42c5869a021
20smukherjee <45695642+20smukherjee@users.noreply.github.com>
2018-12-13 13:15:30 -0500

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,4 +1,6 @@
+import math
 def triangle(leg1,leg2):
-  hyp = sqrt(leg1^2+leg2^2)
+  hyp = sqrt(leg1**2+leg2**2)
   return hyp
 1,2 = user.input("Enter leg 1").int(),user.input("Enter leg2").int()
+triangle(1,2)
